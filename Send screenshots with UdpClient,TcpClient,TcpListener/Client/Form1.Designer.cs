3a755d04f04eaf0bf9ccbbde3646a5b8be085d0b
Amiraslan Aliyev <emiraslaneliyev45@gmail.com>
2022-01-31 21:49:09 +0400

    Add files via upload

---
+++ Send screenshots with UdpClient,TcpClient,TcpListener/Client/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Send = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_Send
+            // 
+            this.btn_Send.Location = new System.Drawing.Point(12, 41);
+            this.btn_Send.Name = "btn_Send";
+            this.btn_Send.Size = new System.Drawing.Size(83, 33);
+            this.btn_Send.TabIndex = 0;
+            this.btn_Send.Text = "Send ";
+            this.btn_Send.UseVisualStyleBackColor = true;
+            this.btn_Send.Click += new System.EventHandler(this.btn_Send_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox.Location = new System.Drawing.Point(101, 43);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(687, 26);
+            this.textBox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.btn_Send);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Send;
+        private System.Windows.Forms.TextBox textBox;
+    }
+}
+
---
+++ Send screenshots with UdpClient,TcpClient,TcpListener/Client/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client
+{
+    public partial class Form1 : Form
+    {
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btn_Send_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var tcpClient = new TcpClient();
+                tcpClient.Connect("127.0.0.1", 45001);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Client error: " + ex.Message);
+            }
+            var client = new UdpClient(45678);
+            var ep = new IPEndPoint(IPAddress.Any, 0);
+
+            while (true)
+            {
+                var bytes = client.Receive(ref ep);
+                var str = Encoding.Default.GetString(bytes);
+                textBox.Text = str;
+
+                Thread.Sleep(5000);
+            }
+        }
+    }
+}
